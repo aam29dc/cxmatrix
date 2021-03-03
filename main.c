f5fc2f3ff3b94f0d204035a92306d9bce65b197a
aam29dc <73267302+aam29dc@users.noreply.github.com>
2021-03-03 15:32:40 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "xadj.h"
 
 /*
- Projects a the vertices of a 3d cube onto the screen using similar triangles.
+ Projects the vertices of a 3d cube onto the screen using similar triangles.
 */
 
 int main(void) {
