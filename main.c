e7c96bb01d995ad62933a90048d31f1a7520b0e1
aam29dc <73267302+aam29dc@users.noreply.github.com>
2021-03-03 14:10:56 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
+#include "xdraw.h"
 #include "xmatrix.h"
-#include <stdlib.h>
+#include "xadj.h"
 
-int main(void) {
+int main(void){
+    MATRIX A = { 0 };
 
-	getchar();
-	return 0;
+    matrix_init(&A,4,4,0);
+    matrix_setList(&A,"1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,");
+    matrix_print(&A,0);
+
+    matrix_REF(&A,0);
+    matrix_print(&A,5);
+
+    return 0;
 }
